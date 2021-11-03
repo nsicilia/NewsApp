d1ea8069388966387b05aa5029bb7ebd5c4afdf5
Nicholas Siciliano <nsicilia@oswego.edu>
2021-11-02 20:13:26 -0400

    Added Json Models

--- NewsApp/APICaller.swift
+++ NewsApp/APICaller.swift
@@ -50,10 +50,15 @@ struct APIResponce: Codable {
 
 struct Article: Codable{
     
+    let  source: Source
     let title: String
     let description:  String
     let url: String
     let urlToImage: String
     let publishedAt: String
     
+}
+
+struct Source: Codable {
+    let name: String
 }
