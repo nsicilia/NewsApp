7008d5e91284768cd8a82b2987ce5bbdfd037657
Nicholas Siciliano <nsicilia@oswego.edu>
2021-11-02 20:02:01 -0400

    adding gitignore

--- NewsApp/APICaller.swift
+++ NewsApp/APICaller.swift
@@ -16,4 +16,44 @@ final class APICaller {
     }
     
     private init() {}
+    
+    public func getTopStories(completion: @escaping (Result<[String],Error>) -> Void){
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, , error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data{
+                do{
+                    let result = try JSONDecoder().decode(String.self, from: data)
+                }
+                catch{
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+        
+    }
+}
+
+//Models
+
+struct APIResponce: Codable {
+    let articles: [Article]
+    
+}
+
+struct Article: Codable{
+    
+    let title: String
+    let description:  String
+    let url: String
+    let urlToImage: String
+    let publishedAt: String
+    
 }
